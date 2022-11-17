2e30dc0d726cef812f65f6934651566b28653010
nakasyou <79000684+nakasyou@users.noreply.github.com>
2022-11-17 18:18:25 +0900

    Create TiCAPCHA.js

---
+++ TiCAPCHA.js
@@ -0,0 +1,106 @@
+function TiCAPCHA(selector,toUrl,time){
+const random={
+  rand:Math.random,
+  chose:lst=>{
+    var len=lst.length
+    var site=Math.floor(Math.random()*lst.length)
+    return lst[site]
+  }
+}
+$(selector).html(`<div class="iAmNot"><div class="riaju" style="padding: 0px; margin-bottom: 0px; border: 0.1px solid #333333;background-color:#fff;position:fixed;bottom:0;right:0;width:100%;height:100%;"><div id="hanbetumondai" style="padding:10px;margin-bottom:10px;background-color:#77f;color:#fff;">直感的に選んでください<br></div><div id="gazoumondai" style="background-color:#fff"></div><br><a id="tugihe" href="javascript:void(0)" style="color:#000">次へ</a></div><div class="kenti" style="padding: 10px; margin-bottom: 10px; border: 0.1px solid #0b0; border-radius: 10px;background-color:#fff;"><span id="okh"><img src="https://cdn.jsdelivr.net/gh/nakasyou/TiCAPCHA@main/ast/ok.png" style="height:20px;" id="ok-r"> <img src="https://cdn.jsdelivr.net/gh/nakasyou/TiCAPCHA@main/ast/no.png" style="height:20px;" id="no-r"> </span><span>私はリア充ではありません</span><br><span id="riaer" style="color:#f00"></span></div></div>`)
+$(".iAmNot .kenti #okh #ok-r").hide()
+$(".iAmNot .riaju").hide()
+$('.iAmNot .kenti #okh #no-r').click(function() {
+    
+    var no=[
+      "https://cdn.jsdelivr.net/gh/nakasyou/TiCAPCHA@main/ast/f/42B8F588-9932-447D-90C4-D4650A233197.jpeg",
+      "https://cdn.jsdelivr.net/gh/nakasyou/TiCAPCHA@main/ast/f/42CDA309-4596-4251-A1E1-82C2E2D76C13.jpeg",
+      "https://cdn.jsdelivr.net/gh/nakasyou/TiCAPCHA@main/ast/f/46B5F016-53B8-4231-B067-7053EC46C603.jpeg",
+      "https://cdn.jsdelivr.net/gh/nakasyou/TiCAPCHA@main/ast/f/48AAA801-5D9D-4225-BBB3-148E5FB8A9C6.jpeg",
+      "https://cdn.jsdelivr.net/gh/nakasyou/TiCAPCHA@main/ast/f/765C1670-0438-4F06-A862-4B9E1E76B669.jpeg",
+    ]
+    var ye=[
+      "https://cdn.jsdelivr.net/gh/nakasyou/TiCAPCHA@main/ast/t/07CE3054-9A3A-4D19-947F-794DE536D3B3.jpeg",
+      "https://cdn.jsdelivr.net/gh/nakasyou/TiCAPCHA@main/ast/t/0FFAB07F-976E-4FA5-B419-2F392302B7E8.jpeg",
+      "https://cdn.jsdelivr.net/gh/nakasyou/TiCAPCHA@main/ast/t/1CD5DD46-AF58-4EFA-ADE3-D7C40A04B92C.jpeg",
+      "https://cdn.jsdelivr.net/gh/nakasyou/TiCAPCHA@main/ast/t/4E18F0A3-4396-4190-86CF-57B11378E0FB.jpeg",
+      "https://cdn.jsdelivr.net/gh/nakasyou/TiCAPCHA@main/ast/t/50FB4555-3FE8-4CEC-9B16-5AE6280D3635.jpeg",
+    ]
+    var al=ye.concat(no)
+    let isRiaju="return ye.includes(url)"
+    let isriaju=new Function("ye","url",isRiaju)
+    
+    
+      $(".iAmNot .riaju").show()
+      let imgcase=$(".iAmNot .riaju #gazoumondai")
+      let noN=0
+      imgcase.html("")
+      for(let i=0;i!=9;i++){
+        
+        var url=random.chose(al)
+        if(!isriaju(ye,url)){
+          noN++
+        }
+        imgcase.append(`<img src="${url}" id="rt${i}" border="0">`)
+        $(".iAmNot .riaju #rt"+i).click(new Function(`
+        function isriaju(url){
+          ${isRiaju}
+        }
+        var i=${i}
+        var url="${url}"
+        var ye=JSON.parse('${JSON.stringify(ye)}')
+        var c=$(".iAmNot .riaju #rt${i}")
+        if(c.attr("border")!="0"){
+          c.attr("border","0")
+        }else{
+          c.attr("border","8")
+        }
+        `))
+        if(i%3==2){
+          imgcase.append("<br>")
+        }
+      }
+      var imgs=$(".iAmNot .riaju #gazoumondai img")
+      imgs.css("width","20%")
+      imgs.css("height","20%")
+      imgs.css("object-fit","cover")
+      $(".iAmNot .riaju #tugihe").click(()=>{
+        useN=0
+        useNn=0
+        for(let i=0;i!=9;i++){
+          let se=$(".iAmNot .riaju #rt"+i)
+          if(se.attr("border")!="0"){
+            if(!isriaju(ye,se.attr("src"))){
+              useN++
+            }else{
+              useNn++
+            }
+          }
+        }
+        let c=noN-useN//ほんらいえらぶべき
+        let score=(useN-useNn-c)/noN
+        if(score<0.5){
+          $(".iAmNot .kenti #riaer").text("エラーが発生しました。あなたはリア充の可能性があります。")
+        }else{
+          $(".iAmNot .kenti #riaer").text()
+          $(".iAmNot .kenti #okh #no-r").hide()
+          $(".iAmNot .kenti #okh #ok-r").show()
+          if(toUrl!=null){
+            if(time==null){
+              time=0
+            }
+            setTimeout(() => {
+            	window.location = toUrl;
+            }, time);
+            location.href=toUrl
+          }
+        }
+        console.log(score)
+        
+        $(".iAmNot .riaju").hide()
+        
+      })
+    
+    
+})
+}
